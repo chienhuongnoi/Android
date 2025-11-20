737b2d1e27cf05c59465a1bbd4c59bbe5edd4bac
chienhuongnoi <nvc2004vp2@gmail.com>
2025-11-20 11:42:13 +0700

    CSDL add Note finish

--- NoteApp/app/src/main/java/com/example/noteapp/AddNoteActivity.kt
+++ NoteApp/app/src/main/java/com/example/noteapp/AddNoteActivity.kt
@@ -1,29 +1,36 @@
 package com.example.noteapp
 
-import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.noteapp.databinding.ActivityMainBinding
+import com.example.noteapp.databinding.ActivityAddNoteBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class AddNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NotesDatabseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        db = NotesDatabseHelper(this)
 
-        binding.addButton.setOnClickListener {
-            val intent = Intent(this, AddNoteActivity::class.java)
-            startActivity(intent)
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+            db.insertNote(note)
+            finish()
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show()
         }
     }
 }
